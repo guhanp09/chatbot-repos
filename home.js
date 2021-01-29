c8fa02029fc3cc23476792235d95eb4dc423ce86
guhanp09 <78214633+guhanp09@users.noreply.github.com>
2021-01-29 19:05:56 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,11 @@
+//var x=prompt("works")
+function msg() {
+    var x = document.getElementById("inputquery").value;
+    if(x=="whatt")
+    {alert("wrong spelling of what")}
+    else
+    {alert(x)}
+
+  }
+
+  
